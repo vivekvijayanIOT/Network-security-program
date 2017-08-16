76a1c3b72b70d76932a50fe5bb756ebd05a087de
VivekIOT <vijayanv31@gmail.com>
2017-08-16 21:25:01 +0530

    Add files via upload
    JAVA program to find Brackets is balanced or not

---
+++ Bracket.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+class Bracket
+{
+public static void main(String args[])
+{	Scanner a=new Scanner(System.in);
+	String s=a.next();
+	System.out.println("Checking the string");
+	String x=s.replace("(","*");
+	String y=x.replace(")",".");
+	int length=s.length();
+	int q=0;
+	String z=".";
+	int count1=0,count2=0;
+	char temp;
+	for( int i = 0; i < s.length( ); i++ )
+	{
+    temp = y.charAt( i );
+    if(temp=='*')
+    	{	
+    		count1=count1+1;
+    	}  
+    if(temp=='.')
+    	{	
+    		count2=count2+1;
+    	}  
+	}
+	
+	if(count1==count2)
+	{
+		System.out.println("Brackets are balanced");
+	}
+	else
+	{
+		System.out.println("Imbalanced bracket");
+	}
+		
+}
+}
